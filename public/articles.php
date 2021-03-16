a0ce8a15a653c03e4adb6024818239d34a32b603
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2021-03-16 13:47:40 +0100

    html css

---
+++ public/articles.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap and CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="css/blog.css" rel="stylesheet">
+
+    <title>Blog Professionnel - Articles</title>
+</head>
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Blog // Corenthin FLAMAND</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php#presentation">Présentation</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="index.php#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="articles.php">Articles</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="login.php">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="register.php">Inscription</a>
+                    </li>
+                </ul>
+            </div>
+         </div>
+    </nav>
+    
+    <div>Articles</div>
+
+</body>
+</html>
