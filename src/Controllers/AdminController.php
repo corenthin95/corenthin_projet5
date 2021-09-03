97737b688fbfda3ea75eccbced7ca3ad24f4b99d
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2021-09-03 12:27:49 +0200

    avancement page admin

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -5,19 +5,58 @@ namespace App\Controllers;
 use App\Application\Http\ParametersBag;
 use App\Application\Http\RedirectResponseHttp;
 use Psr\Http\Message\ServerRequestInterface;
+use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
+use PhpParser\Builder\Param;
 
 class AdminController extends AbstractController
 {
-    public function index(ServerRequestInterface $request, ParametersBag $bag)
-    {
-        if (array_key_exists('user', $_SESSION) && $_SESSION['user']['isAdmin'] !== '1') 
-        {
-            $response = new RedirectResponseHttp('/denied');
-            $response->send();
 
-            return;
+    protected $articleRepository;
+
+    protected $commentRepository;
+
+    public function __construct()
+    {
+        $this->articleRepository = new ArticleRepository();
+        $this->commentRepository = new CommentRepository();
+    }
+
+    public function indexAdmin(ServerRequestInterface $request, ParametersBag $bag)
+    {
+        if (!array_key_exists('user', $_SESSION) || (array_key_exists('user', $_SESSION) && $_SESSION['user']['is_admin'] !== '1')) 
+        {
+            $response = new RedirectResponseHttp('/');
+            $response->send();
         }
 
-        exit;
+        $articles = $this->articleRepository->findAll();
+        
+        return $this->renderHtml(
+            'security/indexAdmin.html.twig',
+            [
+                'articles' => $articles
+            ]
+        );
+    }
+
+    public function addArticle(ServerRequestInterface $request, ParametersBag $bag)
+    {
+        
+    }
+
+    public function validArticle(ServerRequestInterface $request, ParametersBag $bag)
+    {
+
+    }
+
+    public function editArticle(ServerRequestInterface $request, ParametersBag $bag)
+    {
+
+    }
+
+    public function deleteArticle(ServerRequestInterface $request, ParametersBag $bag)
+    {
+
     }
 }
