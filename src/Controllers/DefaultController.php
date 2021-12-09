04ed5cd0ecb92cb2a17aceecf7ea7c495bee807c
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2021-12-09 18:30:51 +0100

    modif

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -21,16 +21,46 @@ class DefaultController extends AbstractController
             // ) {
             //     $errors[]= 'Jeton CSRF invalide.';
             // } else 
-            
             if(strlen($dataSubmitted['name']) > 0 && strlen($dataSubmitted['message']) > 0 && strlen($dataSubmitted['email']) > 0) {
-                $to = 'corenthin.flamand@gmail.com';
-                $headers = ['email' => $dataSubmitted['email']];
-                mail(
-                    $to,
-                    $dataSubmitted['name'],
-                    $dataSubmitted['message'],
-                    $headers
-                );
+                //$to = 'corenthin.flamand@gmail.com';
+                //$headers = ['email' => $dataSubmitted['email']];
+                //    mail(
+                //        $to,
+                //        $dataSubmitted['name'],
+                //        $dataSubmitted['message'],
+                //        $headers
+                //    );
+
+                $mail = new PHPMailer;
+                //Enable SMTP debugging. 
+                $mail->SMTPDebug = 0;
+                //Set PHPMailer to use SMTP.
+                $mail->isSMTP();
+                //Set SMTP Host name                          
+                $mail->Host = $hostname;
+                //Set this to true if SMTP Host requires authentication to send email
+                $mail->SMTPAuth = true;
+                //Provide username and password     
+                $mail->Username = $sender;
+                $mail->Password = $mail_password;
+                //If SMTP requires TLS encryption then set it
+                $mail->SMTPSecure = "ssl";
+                //Set TCP port to connect to 
+                $mail->Port = 465;
+                $mail->From = $sender;  
+                $mail->FromName = $sender_name;
+                $mail->addAddress($to);
+                $mail->isHTML(true);
+                $mail->Subject = $Subject;
+                $mail->Body = $Body;
+                $mail->AltBody = "This is the plain text version of the email content";
+                if (!$mail->send()) {
+                    echo "Mailer Error: " . $mail->ErrorInfo;
+                }
+                else {
+                       echo 'Mail Sent Successfully';
+                }
+
             } else {
                 $errors = 'Tous les champs doivent être remplis.';
             }
