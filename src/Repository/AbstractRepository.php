23b2d4dbea8ed49489dcb65cb0585db7b4bacb2b
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2021-12-21 16:58:00 +0100

    adding readme.md file

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -12,17 +12,12 @@ abstract class AbstractRepository
     {
         $configDatabase = require(__DIR__.'/../../config/database/config_database.php');
         $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $configDatabase['host'], $configDatabase['dbname']);
-       
-        try {
-            $this->database = new PDODatabase(
-                $dsn,
-                $configDatabase['username'],
-                $configDatabase['password']
-            );
-        } catch (\Exception $e) {
-            var_dump($e);
-            exit;
-        }
+        
+        $this->database = new PDODatabase(
+            $dsn,
+            $configDatabase['username'],
+            $configDatabase['password']
+        );
     }
 
     abstract protected function getTableName(): string;
